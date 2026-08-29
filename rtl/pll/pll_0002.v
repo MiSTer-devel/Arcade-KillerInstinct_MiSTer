130d2b32668cbea32ebec08692436dd9f0af317c
Keith Conger <keith.conger@gmail.com>
2026-08-29 14:28:24 -0600

    Set cpu clock to 100 MHz

--- rtl/pll/pll_0002.v
+++ rtl/pll/pll_0002.v
@@ -31,7 +31,7 @@ module  pll_0002(
 		.output_clock_frequency0("50.000000 MHz"),
 		.phase_shift0("0 ps"),
 		.duty_cycle0(50),
-		.output_clock_frequency1("75.000000 MHz"),
+		.output_clock_frequency1("100.000000 MHz"),
 		.phase_shift1("0 ps"),
 		.duty_cycle1(50),
 		.output_clock_frequency2("100.000000 MHz"),
